9864468cf5bad0b032103381a4fba62fe471cdcb
Lien Hoang <kimlienhg@gmail.com>
2015-07-15 22:55:46 +0000

    make return struct more friendly for ffi linking

--- ext/cld/thunk.cc
+++ ext/cld/thunk.cc
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <string>
 #include "internal/lang_script.h"
 #include "public/compact_lang_det.h"
 #include "public/encodings.h"
-#include "internal/"
-
 using namespace CLD2;
 
 typedef struct {
   const char *name;
   const char *code;
-
   bool reliable;
+  //ReturnChunk *re;
 } RESULT;
 
+// Conveys the same information as ReturnChunk, but is more FFI-friendly (for linking to Ruby).
 typedef struct {
-
-} 
+  const char *langcode;
+  const char *content;
+} ReturnChunk;
 
 extern "C" {
+  ReturnChunk convertToReturnChunk(const char* src, ResultChunk* resultchunk) {
+    ReturnChunk rc;
+    rc.langcode = LanguageCode(static_cast<Language>(resultchunk->lang1));
+    rc.content = std::string(src, resultchunk->offset, resultchunk->bytes).c_str(); 
+    return rc;
+  }
+
   RESULT detectLanguageThunkInt(const char * src, bool is_plain_text) {
     const int flags = 0;  // no flags
     const char* tld_hint = NULL;
     const int encoding_hint = UNKNOWN_ENCODING;
     const Language language_hint = UNKNOWN_LANGUAGE;
-    const CLDHints cldhints = {tld_hint, encoding_hint, language_hint};  
+    const CLDHints cldhints = {NULL, tld_hint, encoding_hint, language_hint};  
     Language language3[3];
     int percent3[3];
     double normalized_score3[3];
@@ -41,7 +49,7 @@ extern "C" {
                           language3,
                           percent3,
                           normalized_score3,
-                          &resultchunkvector,
+                          resultchunkvector,
                           &text_bytes,
                           &is_reliable);
 
@@ -52,13 +60,6 @@ extern "C" {
     return res;
   }
 }
-
-  for (int i = 0; i < static_cast<int>(resultchunkvector->size()); ++i) {
-    ResultChunk* rc = &(*resultchunkvector)[i];
-    Language lang1 = static_cast<Language>(rc->lang1);
-    string this_chunk = string(src, rc->offset, rc->bytes);
-    fprintf(f, "[%d]{%d %d %s} ", i, rc->offset, rc->bytes, LanguageCode(lang1));
-  }
 
 int main(int argc, char **argv) {
 }
